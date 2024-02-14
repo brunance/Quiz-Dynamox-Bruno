dc7af85862909e32118e053690f2896742322af6
Bruno França do Prado <bruno_french@outlook.com>
2024-02-14 14:21:08 -0300

    chore: add core files and files to gitignore

---
+++ QuizDynamoxBruno/QuizDynamoxBrunoApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuizDynamoxBrunoApp.swift
+//  QuizDynamoxBruno
+//
+//  Created by Bruno França do Prado on 14/02/24.
+//
+
+import SwiftUI
+
+@main
+struct QuizDynamoxBrunoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
