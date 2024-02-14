a96ca31d21e3c4f5c01c5b213d015a692b19946b
Bruno França do Prado <bruno_french@outlook.com>
2024-02-14 17:09:33 -0300

    chore: add folders for views and components

---
+++ QuizDynamoxBruno/Components/MainButton.swift
@@ -0,0 +1,27 @@
+//
+//  MainButton.swift
+//  QuizDynamoxBruno
+//
+//  Created by Bruno França do Prado on 14/02/24.
+//
+
+import SwiftUI
+
+struct MainButton: View {
+    var text: String
+    var background: Color = Color("AccentColor")
+    
+    var body: some View {
+        Text(text)
+            .foregroundStyle(.white)
+            .padding()
+            .padding(.horizontal)
+            .background(background)
+            .cornerRadius(30)
+            //.shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainButton(text: "Próximo")
+}
